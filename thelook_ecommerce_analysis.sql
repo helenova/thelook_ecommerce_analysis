8e2d0b1d2b7ed37674ce7cec57a1f3cdc6b34d07
Olena Zubeniuk <olenazubeniuk@gmail.com>
2025-04-12 03:12:36 +0300

    Create thelook_ecommerce_analysis.sql

---
+++ thelook_ecommerce_analysis.sql
@@ -0,0 +1,214 @@
+#1.Визначаємо який гендер частіше повертає товар та співвідношення між повернутими та всією кількістю замовлень
+SELECT
+  gender,
+  COUNT(1) AS numb_orders,
+  COUNT(CASE
+      WHEN status='Returned' THEN order_id
+  END
+    ) AS returned_orders,
+  COUNT(CASE
+      WHEN status='Returned' THEN order_id
+  END
+    )/COUNT(1) AS share_returned
+FROM
+  `bigquery-public-data.thelook_ecommerce.orders`
+GROUP BY 1
+ORDER BY 2 DESC;
+
+#2.Визначаємо як змінювався відсоток повернутих замовлень по місяцях
+SELECT
+  DATE_TRUNC(created_at,month) AS order_month,
+  COUNT(oi.id) AS numb_orders,
+  COUNT(CASE
+      WHEN status='Returned' THEN order_id
+  END
+    ) AS returned_orders,
+  COUNT(CASE
+      WHEN status='Returned' THEN order_id
+  END
+    )/COUNT(1) AS share_returned
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items` oi
+GROUP BY 1
+ORDER BY 1 DESC;
+
+#3.Визначаємо яку категорію товарів найбільше повертали
+SELECT
+  p.category,
+  COUNT(oi.id) AS numb_orders,
+  COUNT(CASE
+      WHEN status='Returned' THEN order_id
+  END
+    ) AS returned_orders,
+  COUNT(CASE
+      WHEN status='Returned' THEN order_id
+  END
+    )/COUNT(1) AS share_returned
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items` oi
+LEFT JOIN
+  `bigquery-public-data.thelook_ecommerce.products` p
+ON
+  oi.product_id=p.id
+GROUP BY 1
+ORDER BY 4 DESC;
+
+#4.Визначаємо кількість повернень згідно вікової групи
+SELECT
+  u.gender,
+  CASE
+    WHEN age<21 THEN '<21'
+    WHEN age<35 THEN '21-35'
+    WHEN age<50 THEN '35-50'
+    ELSE '+50'
+  END AS age_group,
+  COUNT(CASE
+      WHEN status='Returned' THEN order_id
+  END
+    )/COUNT(1) AS share_returned
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items` oi
+LEFT JOIN
+  `bigquery-public-data.thelook_ecommerce.users` u
+ON
+  oi.user_id=u.id
+GROUP BY 1,2
+ORDER BY 3 DESC; 
+
+#5.Визначаємо кількість повернень згідно вікової групи з умовою більше 15 тим замовлень
+SELECT
+  u.gender,
+  COUNT(oi.id) AS numb_orders,
+  CASE
+    WHEN age<21 THEN '<21'
+    WHEN age<35 THEN '21-35'
+    WHEN age<50 THEN '35-50'
+    ELSE '+50'
+  END
+  AS age_group,
+  COUNT(CASE
+      WHEN status='Returned' THEN order_id
+  END
+    )/COUNT(1) AS share_returned
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items` oi
+LEFT JOIN
+  `bigquery-public-data.thelook_ecommerce.users` u
+ON
+  oi.user_id=u.id
+GROUP BY 1, 3
+HAVING
+  numb_orders >15000
+ORDER BY 4 DESC; 
+
+#6.Визначаємо профід від користувачів які входять в топ10
+WITH
+  top_ten AS (
+  SELECT
+    user_id,
+    SUM(sale_price)
+  FROM
+    `bigquery-public-data.thelook_ecommerce.order_items` oi
+  WHERE
+    status='Complete'
+  GROUP BY
+    1
+  ORDER BY
+    2 DESC
+  LIMIT
+    10)
+SELECT
+  SUM(oi.sale_price)-SUM(ii.cost) AS profit_from_top3
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items` oi
+LEFT JOIN
+  `bigquery-public-data.thelook_ecommerce.inventory_items` ii
+USING
+  (product_id)
+WHERE
+  oi.status='Complete'
+  AND user_id IN (
+  SELECT
+    user_id
+  FROM
+    top_ten)
+LIMIT
+  10;
+  
+
+#7.Визначаємо чи давати знижку клієнту чи ні (з додаванням фулнейму юзерів)
+SELECT
+  oi.user_id,
+  us.first_name,
+  us.last_name,
+  SUM(oi.sale_price) AS total_sales,
+  AVG(oi.sale_price) OVER() AS avg_total_sales,
+  CASE
+    WHEN SUM(oi.sale_price)>AVG(oi.sale_price) OVER() THEN TRUE
+    ELSE FALSE
+END
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items` oi
+LEFT JOIN
+  `bigquery-public-data.thelook_ecommerce.users` us
+ON
+  oi.user_id=us.id
+GROUP BY
+  oi.user_id,
+  oi.sale_price,
+  us.first_name,
+  us.last_name,
+  oi.status
+HAVING
+  oi.status ='Complete'
+ORDER BY
+  total_sales DESC;
+
+ #8.Визначаємо чи підлягає покупець для надання знижки чи ні (умова що його вартість замовлення вище середньогосередньої) 
+SELECT
+  order_id,
+  user_id,
+  sale_price,
+  AVG(sale_price) OVER() AS avg_total_sales,
+  CASE
+    WHEN sale_price>AVG(sale_price) OVER() THEN TRUE
+    ELSE FALSE 
+  END
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items` oi
+GROUP BY
+  1,2,3;
+
+#9.Визначаємо 5% відсотків найкращих покупців яким ми можемо запропонувати знижку
+WITH orders AS (
+  SELECT
+  user_id,
+  SUM(sale_price) as sum_order
+FROM
+  `bigquery-public-data.thelook_ecommerce.order_items`
+GROUP BY 1),
+ranks AS (
+  SELECT
+  *,
+  PERCENT_RANK() OVER(ORDER BY orders.sum_order) as persent_rank
+FROM orders
+)
+SELECT
+*
+FROM ranks
+WHERE persent_rank>=0.95
+
+#Порівнюємо суму кожного замовлення з сумою першого замовлення
+WITH first_orders AS (
+  SELECT 
+    *,
+    ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY created_at) AS rn,
+    FIRST_VALUE(sale_price) OVER(PARTITION BY user_id ORDER BY created_at) AS first_price
+  FROM `bigquery-public-data.thelook_ecommerce.order_items`
+)
+
+SELECT
+  AVG(first_price - sale_price) AS avg_diff,
+  AVG(first_price - sale_price) / AVG(first_price) AS avg_prc_diff
+FROM first_orders
+WHERE status = 'Complete'
